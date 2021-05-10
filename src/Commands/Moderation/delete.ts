bb80f155280a6b7b5f2174f5a4d12f303abaecab
W1kipedia <jrizzo2007@gmail.com>
2021-05-10 12:19:22 -0500

    added delete.ts

---
+++ src/Commands/Moderation/delete.ts
@@ -0,0 +1,23 @@
+import { TextChannel } from 'discord.js';
+import { Command } from '../../Interfaces';
+
+export const command: Command = {
+    "name": "delete",
+    "description": "Delete an amount of messages from a channel",
+    "aliases": ["del", "purge"],
+    run: async (client, msg, args) => {
+        if (args.length === 0) {const amount: number = 2};
+
+        const amount: number = parseInt((args[0] as string));
+        if (!(typeof amount === 'number')) return;
+        
+        if (msg.guild.member(msg.author).hasPermission('MANAGE_MESSAGES')) {
+
+            if (msg.channel.type === 'text') msg.channel.bulkDelete(amount);
+
+        } else {
+            msg.channel.send("❌ You don't have permissions. ❌");
+        }
+
+    }
+}
