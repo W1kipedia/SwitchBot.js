6b1b3b62016029466f52e8909891b25dcc697dd8
W1kipedia <jrizzo2007@gmail.com>
2021-05-12 13:36:08 -0500

    added cryson.ts
    this is another command I'm thinking about deleting

---
+++ src/Commands/Images/cryson.ts
@@ -0,0 +1,22 @@
+import { Emoji, GuildEmoji, MessageEmbed } from 'discord.js';
+import { Command } from '../../Interfaces';
+
+export const command: Command = {
+    name: "cole",
+    description: "cole moment",
+    aliases: [],
+    run: async (client, msg, args) => {
+        const e = new MessageEmbed({ title: "cole moment" });
+        e.setFooter(`Command used by ${msg.author.username}`, msg.author.avatarURL());
+        e.setImage('https://media.tenor.com/images/0000de7195a4d303add46b3928ee509f/tenor.gif');
+        var joe = undefined;
+        msg.guild.emojis.cache.forEach(emoji => {
+            if (emoji.name === 'joe') joe = emoji;
+        });
+        if (typeof joe === 'undefined') joe = '🦧';
+
+        await msg.channel.send(e)
+        .then((m) => m.delete({timeout: 3000, reason: "stop looking at audit logs >_>"}));
+        await msg.react(joe);
+    }
+}
