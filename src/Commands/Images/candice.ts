9340205a2d56f911ce801533e5d5d73563847f2c
W1kipedia <jrizzo2007@gmail.com>
2021-05-12 13:11:29 -0500

    added all the funni cats

---
+++ src/Commands/Images/candice.ts
@@ -0,0 +1,14 @@
+import { MessageEmbed } from 'discord.js';
+import { Command } from '../../Interfaces';
+
+export const command: Command = {
+    name: "candice",
+    description: "candice moment",
+    aliases: [],
+    run: async (client, msg, args) => {
+        const e = new MessageEmbed({ title: "candice moment" });
+        e.setFooter(`Command used by ${msg.author.username}`, msg.author.avatarURL());
+        e.setImage('https://media.discordapp.net/attachments/693942287910305842/753749038356103318/cat_next_to_camera2.jpg');
+        msg.channel.send(e);
+    }
+}
---
+++ src/Commands/Images/joe.ts
@@ -0,0 +1,14 @@
+import { MessageEmbed } from 'discord.js';
+import { Command } from '../../Interfaces';
+
+export const command: Command = {
+    name: "joe",
+    description: "joe moment",
+    aliases: [],
+    run: async (client, msg, args) => {
+        const e = new MessageEmbed({title: "Joe moment"});
+        e.setFooter(`Command by ${msg.author.username}`, msg.author.avatarURL())
+        e.setImage('https://media.discordapp.net/attachments/693941496147214357/753068754228871309/cat_next_to_camera.jpg');
+        msg.channel.send(e);
+    }
+}
