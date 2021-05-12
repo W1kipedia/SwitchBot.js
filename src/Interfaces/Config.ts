1caa8984f13a588f6003924044e091a22528e1d4
W1kipedia <jrizzo2007@gmail.com>
2021-05-12 02:01:04 -0500

    Added role.ts and updated config

--- src/Interfaces/Config.ts
+++ src/Interfaces/Config.ts
@@ -1,8 +1,14 @@
 export interface Config {
     token: string,
     prefix: string,
-    dbEconomy: {host:string,
+    dbEconomy: {
+        host:string,
         database:string,
         user:string
-        password:string}
+        password:string},
+    dbAll: {
+        host: string,
+        database: string,
+        user: string
+        password: string}
 }
