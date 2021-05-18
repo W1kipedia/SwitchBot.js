7b2b3f8de44b5d976e7313bb0c2fafb1e8a65720
W1kipedia <jrizzo2007@gmail.com>
2021-05-18 12:56:14 -0500

    added wikipedia_summary.ts and wikipedia_search.ts

---
+++ src/Commands/Wikipedia/wikipedia_summary.ts
@@ -0,0 +1,64 @@
+import { Command } from "../../Interfaces";
+import wikipedia from 'wikipedia';
+import { MessageEmbed } from "discord.js";
+
+export const command: Command = {
+    name: 'wikipedia_summary',
+    description: 'Get a summary of what wikipedia article you would like!',
+    aliases: ['wiki_summary', 'summary'],
+    run: async (client, msg, args) => {
+        if (args.length === 0) return;
+
+        if (msg.channel.id === '693942287910305842') {
+            wikipedia.page(args.toString().replace(/,/g, ' '))
+            .then(async (page) => {
+                const summary = (await page.summary()).extract;
+
+                if (summary.toLowerCase().endsWith('may refer to:')) {
+                    wikipedia.search(args.toString().replace(/,/g, ' '), { limit: 20 })
+                        .then((srch) => {
+                            const results: string[] = []
+
+                            srch.results.forEach((result) => {
+                                results.push(result.title)
+                            });
+
+                            const e = new MessageEmbed({
+                                title: 'Wikipedia Search',
+                                color: 3066993,
+                                fields: [
+                                    { name: '**results**', value: results.toString().replace(/,/g, '\n'), inline: true },
+                                ],
+                                footer: { text: `Command by ${msg.author.username}`, iconURL: msg.author.avatarURL() }
+                            });
+                            e.setThumbnail('https://media.discordapp.net/attachments/756027330656337951/844262888097185802/1200px-Wikipedia-logo-v2.png');
+                            msg.channel.send('I ran into a disambiguation!\n search one of these instead:', {embed: e});
+                        })
+                        .catch((err) => {
+                            msg.channel.send("I see to have run into an error, displaying error!\n`" + err + "`");
+                        });
+                        return;
+                }
+
+                const em = new MessageEmbed({
+                    title: page.title,
+                    url: page.fullurl,
+                    color: 3066993,
+                    footer: {text: `Command by ${msg.author.username}`, iconURL: msg.author.avatarURL()},
+                    fields: [
+                        {name: 'Wikipedia Summary', value: summary, inline: true}
+                    ]
+                });
+                em.setThumbnail('https://media.discordapp.net/attachments/756027330656337951/844262888097185802/1200px-Wikipedia-logo-v2.png');
+                msg.channel.send(em);
+            })
+            .catch(async (err) => {
+                msg.channel.send("Couldn't find the page `" + args.toString().replace(/,/g, ' ') + "`, maybe try searching for it?");
+            })
+
+        } else {
+            msg.channel.send("you can only use this command in <#693942287910305842>");
+        }
+
+    }
+}
