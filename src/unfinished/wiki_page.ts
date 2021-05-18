c43f0163c47b315e57b5897fe803efbb4539af75
W1kipedia <jrizzo2007@gmail.com>
2021-05-18 12:57:21 -0500

    added unfinished folder
    the only reason why I wouldn't make another branch and name it 'unstable' or something is because this whole repo is unstable right now since it isn't finished. I will complete this command once I stablize all the commands

---
+++ src/unfinished/wiki_page.ts
@@ -0,0 +1,36 @@
+import { Command } from "../../Interfaces";
+import wikipedia from 'wikipedia';
+import { MessageEmbed } from "discord.js";
+
+export const command: Command = {
+    name: 'wiki_page',
+    description: 'Get the entire page content of an article (warning, this may not work all the time due to too many characters)',
+    aliases: ['page'],
+    run: async (client, msg, args) => {
+        if (args.length === 0) return;
+
+        if (msg.channel.id === '693942287910305842') {
+            wikipedia.page(args.toString().replace(',', ' '))
+                .then(async (page) => {
+                    const em = new MessageEmbed({
+                        title: page.title,
+                        url: page.fullurl,
+                        color: 3066993,
+                        footer: { text: `Command by ${msg.author.username}`, iconURL: msg.author.avatarURL() },
+                        fields: [
+                            { name: 'summary', value: page.content(), inline: true }
+                        ]
+                    });
+                    msg.channel.send(em);
+                })
+                .catch(async (err) => {
+                    console.log(typeof err);
+                    console.error(err);
+                    msg.channel.send("Couldn't find the page `" + args.toString().replace(',', ' ') + "`, maybe try searching for it?");
+                })
+
+        } else {
+            msg.channel.send("you can only use this command in <#693942287910305842>")
+        }
+    }
+}
