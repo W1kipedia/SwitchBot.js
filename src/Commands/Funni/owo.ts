23da9b3e75799c8c136887596466597358a65de1
W1kipedia <jrizzo2007@gmail.com>
2021-05-20 12:37:06 -0500

    added owo.ts

---
+++ src/Commands/Funni/owo.ts
@@ -0,0 +1,36 @@
+import { Command } from '../../Interfaces';
+
+export const command: Command = {
+    name: 'owo',
+    description: 'owoify any text',
+    aliases: [],
+    run: async (client, msg, args) => {
+        if (args.length === 0) return;
+
+        const message: string = args.toString().replace(/,/g, ' ');
+        var output: string = "";
+
+        for (let i = 0; i < message.length; i++) {
+            switch (message.charAt(i).toLowerCase()) {
+
+                case 'o':
+                    output = output + 'OwO';
+                    break;
+
+                case 'u':
+                    output = output + 'UwU'
+                    break;
+
+                default:
+                    output = output + message.charAt(i);
+                    break;
+            }
+        }
+
+        msg.channel.send(`Text: \`${message}\`\nTranslated: \`${output}\``)
+        .catch((err) => {
+            console.error(err);
+        });
+
+    }
+}
