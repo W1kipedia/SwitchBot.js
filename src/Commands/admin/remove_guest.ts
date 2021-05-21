71715b6a64133ee25d853f41ae26dd79e6efb3ea
W1kipedia <jrizzo2007@gmail.com>
2021-05-21 09:23:17 -0500

    added remove_guest.ts

---
+++ src/Commands/admin/remove_guest.ts
@@ -0,0 +1,28 @@
+import { Command } from '../../Interfaces';
+import { readFile } from 'fs/promises';
+
+export const command: Command = {
+    name: 'remove_guest',
+    aliases: ['unguest', 'revoke_guest'],
+    run: async (client, msg, args) => {
+        if (!(msg.author.id === client.config.id.owner.toString())) return;
+        
+        readFile('/tmp/guest.txt', {encoding: 'utf-8'})
+        .then((data) => {
+            msg.guild.members.cache.forEach((member) => {
+                if (member.id === data) {
+                    const guest = member;
+                    guest.roles.remove(data);
+                    msg.channel.send(`${guest.user.username} is no longer a guest`)
+                    .catch(err => {
+                        throw new Error(err);
+                    })
+                }
+            })
+        })
+        .catch((err) => {
+            throw new Error(err);
+        });
+
+    }
+}
