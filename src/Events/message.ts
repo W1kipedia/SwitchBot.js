f1cbf3cd94a3e3c98e8ff59a1f11acc186e1d520
W1kipedia <jrizzo2007@gmail.com>
2021-05-21 10:07:57 -0500

    added functionality to the guest system

--- src/Events/message.ts
+++ src/Events/message.ts
@@ -1,11 +1,75 @@
 import { Events, Command } from '../Interfaces'
 import { Message } from 'discord.js';
+import { readFile, unlink } from 'fs/promises';
 
 export const event: Events = {
     name: 'message',
     run: (client, msg: Message) => {
-        if (!msg.guild || !msg.content.toLocaleLowerCase().startsWith(client.config.prefix))
-        return;
+        if (!msg.guild) return; 
+        if (msg.channel.id === '738155429342871623') {
+            if (msg.author.id === client.config.id.owner.toString()) {
+                msg.react('👍')
+                    .then(() => {
+                        msg.react('👎')
+                            .then(() => {
+                                msg.react("🤔")
+                                    .catch((err) => {
+                                        console.error(err);
+                                    })
+                            })
+                            .catch((err) => {
+                                console.error(err);
+                            })
+                    })
+                    .catch((err) => {
+                        console.error(err);
+                    })
+            }
+            readFile('/tmp/guest.txt', {encoding: 'utf-8'})
+            .then((data) => {
+                msg.guild.members.cache.forEach((member) => {
+                    if (member.id === data) {
+                        msg.react('👍')
+                            .then(() => {
+                                msg.react('👎')
+                                    .then(() => {
+                                        msg.react("🤔")
+                                            .catch((err) => {
+                                                console.error(err);
+                                            })
+                                    })
+                                    .catch((err) => {
+                                        console.error(err);
+                                    })
+                            })
+                            .catch((err) => {
+                                console.error(err);
+                            })
+                        member.roles.remove(client.config.id.guestRole.toString())
+                            .then(() => {
+                                console.log(`${member.user.username} is no longer a guest`);
+                            })
+                            .catch((err) => {
+                                console.error(err);
+                            });
+                    }
+                })
+            })
+            .then(() => {
+                unlink('/tmp/guest.txt')
+                    .then(() => {
+                        console.log("successfully delete /tmp/guest.txt");
+                    })
+                    .catch((err) => {
+                        console.error(err);
+                    })
+            })
+            .catch(async (err) => {
+                await msg.delete();
+                console.error(err);
+            })
+        }
+        if (!msg.content.toLocaleLowerCase().startsWith(client.config.prefix))  return;
         
         const args = msg.content
             .slice(client.config.prefix.length)
