15b2faa6bcc90a9c5344641e95558d1837039e84
W1kipedia <jrizzo2007@gmail.com>
2021-05-23 02:59:08 -0500

    added join.ts event listener

---
+++ src/Events/join.ts
@@ -0,0 +1,22 @@
+import { GuildEmoji, GuildMember, TextChannel } from 'discord.js';
+import { Events } from '../Interfaces';
+
+export const event: Events = {
+    name: 'guildMemberAdd',
+    run: (client, member:GuildMember) => {
+        console.log(`${member.user.username} has joined the server!`);
+        var kirby: GuildEmoji = undefined;
+
+        member.guild.emojis.cache.forEach((emoji:GuildEmoji) => {
+            if (emoji.id === '845826814865047572') kirby = emoji;
+        });
+
+        member.guild.channels.cache.filter(c => c.type === 'text')
+        .forEach((channel:TextChannel) => {
+            if (channel.id === '693942943039488050') {
+                channel.send(`${member.user.username} has joined the arena!`)
+                    .catch(err => console.error(err))
+            }
+        })
+    }
+}
