98c9a3ebce208d7f5a2adbb9d37d38cd73a0e68d
W1kipedia <jrizzo2007@gmail.com>
2021-05-23 22:06:57 -0500

    deleted gambling.ts and added gambleflip.ts and gambling category

---
+++ src/Commands/gambling/gambleflip.ts
@@ -0,0 +1,39 @@
+import { Command } from '../../Interfaces';
+import { createConnection } from 'mysql';
+
+export const command: Command = {
+    name: 'gambleflip',
+    description: 'it\'s like coinflipping but gambling',
+    aliases: [],
+    run: async (client, msg, args) => {
+        if (args.length === 0) return;
+        try{const amount = parseInt((args[0] as string))}
+        catch (error) {
+            msg.channel.send("You must provide an amount to gamble!")
+                .catch((err) => console.error(err));
+            return;
+        }
+        await client.OpenAccount(msg.author.id);
+        const choices = [true, false];
+
+        const conn = createConnection(client.config.dbEconomy);
+
+        conn.connect((err) => {
+            if (err) throw err;
+
+            if (choices[Math.floor(Math.random() * choices.length)]) {
+                const earnings = Math.floor(Math.random() * 100);
+
+                msg.channel.send(`You made ${earnings} snips!`)
+                    .then(() => {
+                        const q = `UPDATE Economy SET wallet = ${earnings} WHERE client_id = '${msg.author.id}'`;
+                        conn.query(q, (err) => {if (err) throw err});
+                    })
+                    .catch(err => console.error(err));
+            } else {
+                msg.channel.send("after begging for some money you weren't able to get any money 😔")
+                    .catch((err) => console.error(err))
+            }
+        })
+    }
+}
