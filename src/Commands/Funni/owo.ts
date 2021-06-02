9f083abf64081a2f86f03833b40a6f94c77654e3
W1kipedia <jrizzo2007@gmail.com>
2021-06-02 01:06:17 -0500

    Helped specification with owo command

--- src/Commands/Funni/owo.ts
+++ src/Commands/Funni/owo.ts
@@ -3,11 +3,14 @@ import { Command } from '../../Interfaces';
 export const command: Command = {
     name: 'owo',
     description: 'owoify any text',
-    example: 's.owo This Text is owoified!',
+    example: 's.owo [Message]',
     public: true,
     aliases: [],
     run: async (client, msg, args) => {
-        if (args.length === 0) return;
+        if (args.length === 0) {
+            msg.channel.send("use `s.info owo` for info on how to use this command");
+            return;
+        }
 
         const message: string = args.toString().replace(/,/g, ' ');
         var output: string = "";
