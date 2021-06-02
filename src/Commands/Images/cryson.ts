422694aaef82b648daa7b1bf4821799a806de5a2
W1kipedia <jrizzo2007@gmail.com>
2021-06-02 10:44:21 -0500

    Fixed stupid bug with the object props

--- src/Commands/Images/cryson.ts
+++ src/Commands/Images/cryson.ts
@@ -2,13 +2,12 @@ import { Emoji, GuildEmoji, MessageEmbed } from 'discord.js';
 import { Command } from '../../Interfaces';
 
 export const command: Command = {
-    name: "cole",
-    description: "cole moment",
+    name: "cryson",
     example: 's.cryson',
     public: true,
     aliases: [],
     run: async (client, msg, args) => {
-        const e = new MessageEmbed({ title: "cole moment" });
+        const e = new MessageEmbed({ title: "Stop using cryson" });
         e.setFooter(`Command used by ${msg.author.username}`, msg.author.avatarURL());
         e.setImage('https://media.tenor.com/images/0000de7195a4d303add46b3928ee509f/tenor.gif');
         var joe = undefined;
