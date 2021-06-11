63f9ad496363e916649b149c0f9afa036e4d7b6c
W1kipedia <jrizzo2007@gmail.com>
2021-06-11 18:39:16 -0500

    Can't use present_guest

--- src/Commands/admin/present_guest.ts
+++ src/Commands/admin/present_guest.ts
@@ -7,20 +7,24 @@ export const command: Command = {
     aliases: ['guest', 'add_guest'],
     public: false,
     run: async (client, msg, args) => {
-        if (!(msg.author.id === client.config.id.owner.toString())) return;
-        if (args.length === 0) {msg.channel.send("You must mention someone to add them as a guest!"); return;};
-        const guest: GuildMember = msg.mentions.members.first();
-        const guestRole = await msg.guild.roles.fetch(`${client.config.id.guestRole}`)
-        
-        writeFile("/tmp/guest.txt", guest.id)
-        .then(() => {
-            console.log(`successfully written ${guest.id} to /tmp/guest.txt`)
-        })
-        .catch((err) => {
-            if (err) {msg.channel.send(`I've run into an error!\nerror: \`${err}\``); return;}
-        })
 
-        guest.roles.add(guestRole)
-        msg.channel.send(`** ${guest.user.username} **, remember that\n\`\`\`\n- you'll only have one message to send in the chat, which means your guest role will be taken away immediately after. So make sure there's no typos!\n\n- your message must be in the same format as any other big brain thoughts (you can also add a thing like \"<insert your name> DLC\" right after the bottom line if ya want)\n\`\`\``)
+        msg.channel.send("This command currently doesn't exist");
+
+    //     if (!(msg.author.id === client.config.id.owner.toString())) return;
+    //     if (args.length === 0) {msg.channel.send("You must mention someone to add them as a guest!"); return;};
+    //     const guest: GuildMember = msg.mentions.members.first();
+    //     const h: `${bigint}` = `${BigInt(832068361932111872)}`;
+    //     const guestRole = await msg.guild.roles.fetch(h)
+        
+    //     writeFile("/tmp/guest.txt", guest.id)
+    //     .then(() => {
+    //         console.log(`successfully written ${guest.id} to /tmp/guest.txt`)
+    //     })
+    //     .catch((err) => {
+    //         if (err) {msg.channel.send(`I've run into an error!\nerror: \`${err}\``); return;}
+    //     })
+
+    //     guest.roles.add(guestRole)
+    //     msg.channel.send(`** ${guest.user.username} **, remember that\n\`\`\`\n- you'll only have one message to send in the chat, which means your guest role will be taken away immediately after. So make sure there's no typos!\n\n- your message must be in the same format as any other big brain thoughts (you can also add a thing like \"<insert your name> DLC\" right after the bottom line if ya want)\n\`\`\``)
     }
 }
