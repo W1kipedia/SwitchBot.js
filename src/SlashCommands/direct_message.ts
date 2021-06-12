4f2787293d32061aebd0f6b835e0ea9777cd340d
W1kipedia <jrizzo2007@gmail.com>
2021-06-11 20:52:16 -0500

    Added direct_messages to slash commands

---
+++ src/SlashCommands/direct_message.ts
@@ -0,0 +1,27 @@
+import { SlashCommand } from '../Interfaces';
+
+export const slashCommand: SlashCommand = {
+	run: async (interaction) => {
+		console.log(interaction.user.id);
+		if (interaction.guild.members.cache.get(interaction.user.id).roles.cache.has('643128678804619316')) {
+			const person_to_send_msg_to = interaction.guild.members.cache.get(interaction.options.array()[0].user.id);
+
+			person_to_send_msg_to.user.send({
+				content: `A moderator from Switch n' Snap decided to anonymously dm you \`${interaction.options.array()[1].value.toString()}\``
+			})
+			.then(msg => {
+				console.log(`${interaction.user.tag} sent ${interaction.options.array()[0].user.tag} "${interaction.options.array()[1].value.toString()}"`);
+				interaction.reply({
+					content: "✅Succesfully sent!",
+					ephemeral: true
+				});
+			})
+			.catch(err => {
+				interaction.reply({
+					content: "The person you're trying to dm has their dms closed :c",
+					ephemeral: true
+				})
+			});
+		}
+	}
+}
