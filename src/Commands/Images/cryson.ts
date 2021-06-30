570733af17d6e535c00a95330bd010f1e1875fa0
W1kipedia <jrizzo2007@gmail.com>
2021-06-30 12:57:02 -0500

    Changed s.cryson back to original

--- src/Commands/Images/cryson.ts
+++ src/Commands/Images/cryson.ts
@@ -7,17 +7,10 @@ export const command: Command = {
     public: true,
     aliases: [],
     run: async (client, msg, args) => {
-        const e = new MessageEmbed({ title: "Stop using cryson" });
+        const e = new MessageEmbed();
         e.setFooter(`Command used by ${msg.author.username}`, msg.author.avatarURL());
-        e.setImage('https://media.tenor.com/images/0000de7195a4d303add46b3928ee509f/tenor.gif');
-        var joe = undefined;
-        msg.guild.emojis.cache.forEach(emoji => {
-            if (emoji.name === 'joe') joe = emoji;
-        });
-        if (typeof joe === 'undefined') joe = '🦧';
+        e.setImage('https://media1.tenor.com/images/61e127273f4ce7c6cefcdfa07183f0a0/tenor.gif');
 
         msg.channel.send({ embed: e })
-        .then((m) => setTimeout(() => m.delete(), 3000))
-        await msg.react(joe);
     }
 }
