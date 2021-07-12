485e46df7c9cf70fcde9f5cab9acc82cba64cf01
W1kipedia <jrizzo2007@gmail.com>
2021-07-12 17:54:04 -0500

    Fixed intents problem

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@ import Client from './Client';
 
 const e = new Client({
     intents: [
-        "GUILD_MESSAGES"
+        'GUILDS',
+        'GUILD_PRESENCES',
+        'GUILD_EMOJIS',
+        'GUILD_BANS',
+        'GUILD_MEMBERS',
+        'GUILD_MESSAGE_REACTIONS',
+        'GUILD_MESSAGES',
+        'GUILD_VOICE_STATES',
+        'GUILD_INTEGRATIONS'
     ]
 });e.init();
