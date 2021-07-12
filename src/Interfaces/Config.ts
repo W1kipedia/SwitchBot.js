1c404b784e4d3bf6c367868b7a7117497d0aaf07
W1kipedia <jrizzo2007@gmail.com>
2021-07-12 18:33:06 -0500

    Forgot this was important lol

--- src/Interfaces/Config.ts
+++ src/Interfaces/Config.ts
@@ -1,6 +1,18 @@
 export interface Config {
     token: string,
     prefix: string,
+    dbEconomy: {
+        host: string,
+        database: string,
+        user: string,
+        password: string
+    },
+    dbAll: {
+        host: string,
+        database: string,
+        user: string,
+        password: string
+    },
     id: {
         owner : number,
         guestRole: number
