79ecf45400f393dc02aa3f36de7f536e6bdd2555
W1kipedia <jrizzo2007@gmail.com>
2021-07-19 18:41:35 -0500

    Updated direct_message with discord.js syntax

--- src/SlashCommands/direct_message.ts
+++ src/SlashCommands/direct_message.ts
@@ -3,13 +3,13 @@ import { SlashCommand } from '../Interfaces';
 export const slashCommand: SlashCommand = {
 	run: async (client, interaction) => {
 		if (interaction.guild.members.cache.get(interaction.user.id).roles.cache.has('643128678804619316')) {
-			const person_to_send_msg_to = interaction.guild.members.cache.get(interaction.options.array()[0].user.id);
+			const person_to_send_msg_to = interaction.guild.members.cache.get(interaction.options.getUser('member').id);
 
 			person_to_send_msg_to.user.send({
-				content: `A moderator from Switch n' Snap decided to anonymously dm you \`${interaction.options.array()[1].value.toString()}\``
+				content: `A moderator from Switch n' Snap decided to anonymously dm you \`${interaction.options.getString('message').toString()}\``
 			})
 			.then(msg => {
-				client.UpdateLog(`${interaction.user.tag} sent ${interaction.options.array()[0].user.tag} "${interaction.options.array()[1].value.toString()}"`);
+				client.UpdateLog(`${interaction.user.tag} sent ${interaction.options.getUser('member').tag} "${interaction.options.getString('message').toString()}"`);
 				interaction.reply({
 					content: "✅Succesfully sent!",
 					ephemeral: true
