2a091532334ab14205ac23692c44a10fd31d7e93
W1kipedia <jrizzo2007@gmail.com>
2021-08-13 13:48:35 -0500

    Finally fixed s.join

--- src/Commands/Tts/join.ts
+++ src/Commands/Tts/join.ts
@@ -1,4 +1,5 @@
 import { Command } from '../../Interfaces';
+import { joinVoiceChannel, getVoiceConnection } from '@discordjs/voice'
 
 export const command: Command = {
     name: 'join',
@@ -7,43 +8,35 @@ export const command: Command = {
     public: true,
     aliases: [],
     run: async (client, msg, args) => {
-
-        msg.channel.send("Currently cannot work due to discord.js v13 not being finished :c")
-
-        // if (msg.member.voice.channel) {
-        //     if (!msg.guild.members.cache.get(client.user.id).voice.channel) {
-        //         if (msg.member.voice.channel.type === 'voice' && msg.member.voice.channel.joinable) {
-        //             msg.member.voice.channel
-        //             client.voice.client
-        //         }
-        //         msg.react('🤝')
-        //         .catch((err) => {
-        //             console.error(err);
-        //         })
-        //     } else {
-        //         client.voice.adapters.
-        //         forEach(async (channel) => {
-        //             if (channel.channel.guild.id === msg.guild.id) {
-        //                 if (channel.channel.members.array().length > 1) msg.channel.send("I'm already in a voice channel with others :c");
-        //                 else {
-        //                     channel.disconnect();
-        //                     msg.react('🤝')
-        //                        .catch((err) => {
-        //                             msg.channel.send(err);
-        //                         });
-        //                     await msg.member.voice.channel.join()
-        //                     .catch(async err => {
-        //                         msg.channel.send("it seems I have ran into a problem while joining, mind trying the command again?");
-        //                         msg.react('❌')
-        //                         .catch((err) => {
-        //                             msg.channel.send(err);
-        //                         });
-        //                         console.error(err);
-        //                     });
-        //                 }
-        //             }
-        //         });
-        //     }
-        // } else msg.channel.send("You must be in a voice channel to use this command!")
+        if (msg.member.voice.channel) {
+            if (!msg.guild.members.cache.get(client.user.id).voice.channel) {
+                if (msg.member.voice.channel.type === 'GUILD_VOICE' && msg.member.voice.channel.joinable) {
+                    joinVoiceChannel({
+                        channelId: msg.member.voice.channel.id,
+                        guildId: msg.guild.id,
+                        adapterCreator: msg.guild.voiceAdapterCreator
+                    })
+                }
+                msg.react('🤝')
+                .catch((err) => {
+                    console.error(err);
+                })
+            } else {
+                const voiceChannel = msg.guild.members.cache.get(client.user.id).voice.channel
+                if (voiceChannel.members.size > 1) msg.channel.send("I'm already in a voice channel with others :c");
+                else {
+                    getVoiceConnection(msg.guild.id).destroy();
+                    msg.react('🤝')
+                        .catch((err) => {
+                            msg.channel.send(err);
+                        });
+                    joinVoiceChannel({
+                        channelId: msg.member.voice.channel.id,
+                        guildId: msg.guild.id,
+                        adapterCreator: msg.guild.voiceAdapterCreator
+                    })
+                }
+            }
+        } else msg.channel.send("You must be in a voice channel to use this command!")
     }
 }
